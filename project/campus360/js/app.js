908cd9c924f0def21cbb61600ea64455afd117bc
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-23 09:59:01 +0530

    Form added

--- project/campus360/js/app.js
+++ project/campus360/js/app.js
@@ -1,22 +1,29 @@
 var app = angular.module("campus360", []);
 
 app.controller("dashboardController", function ($scope) {
-  $scope.collegename = "FISAT";
-  $scope.totalstudents = 1200;
-  $scope.totalfaculty = 85;
+    $scope.collegename = "FISAT";
+    $scope.collegeId = "FISAT-2026";
+    $scope.totalstudents = 1200;
+    $scope.totalfaculty = 85;
 
-  $scope.students = ["Shahid", "John", "Allen", "Shaun"];
+    $scope.students = ["Shahid", "John", "Allen", "Shaun"];
+    $scope.showstudents = true;
 
-  $scope.fees = 2500;
-  $scope.today = new Date();
+    $scope.fees = 2500;
+    $scope.today = new Date();
 
-  $scope.departments = ["CSE", "MCA", "MBA"];
+    $scope.departments = ["CSE", "MCA", "MBA", "EEE", "ECE"];
+    $scope.selectedDepartment = "CSE";
 
-  $scope.selectedDepartment = "CSE";
+    // Actions
+    $scope.addStudent = function () {
+        $scope.totalstudents++;
+    };
 
-  $scope.showstudents = true;
+    $scope.isDisable = true;
+    $scope.isReadOnly = true;
 
-  $scope.addStudent = function () {
-    $scope.totalstudents++;
-  };
+    $scope.allowAdmission = function () {
+        $scope.isDisable = !$scope.isDisable;
+    };
 });
